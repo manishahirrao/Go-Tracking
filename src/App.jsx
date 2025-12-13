ca905fd305c4e768822b81c39c76309b6f5fcc80
Manish <manishkishor2003@gmail.com>
2025-12-14 00:08:13 +0530

    404 fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { lazy, Suspense } from 'react';
 import Layout from './components/Layout';
 import ErrorBoundary from './components/common/ErrorBoundary/ErrorBoundary';
